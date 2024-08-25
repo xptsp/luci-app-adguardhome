a370e2121e75cb32f10efc78639bff0da51a9885
Douglas Orend <douglas.orend@comcast.net>
2024-08-25 16:55:07 -0500

    Version 2.1
    Added ability to change AdGuardHome username and pasword from LuCI WebUI

--- Makefile
+++ Makefile
@@ -5,8 +5,8 @@ include $(TOPDIR)/rules.mk
 
 PKG_LICENSE:=Apache-2.0
 PKG_NAME:=luci-app-adguardhome
-PKG_VERSION:=2.0.2
-PKG_RELEASE:=2
+PKG_VERSION:=2.1
+PKG_RELEASE:=1
 PKG_MAINTAINER:=Douglas Orend <doug.orend2@gmail.com>
 
 PKG_BUILD_DIR:=$(BUILD_DIR)/$(PKG_NAME)
@@ -34,10 +34,10 @@ endef
 
 define Package/luci-app-adguardhome/install
 	$(INSTALL_DIR) $(1)/www/luci-static/resources/view/adguardhome/
-	$(INSTALL_DATA) ./htdocs/luci-static/resources/view/adguardhome/*.js $(1)/www/luci-static/resources/view/adguardhome/
+	$(INSTALL_DATA) ./htdocs/*.js $(1)/www/luci-static/resources/view/adguardhome/
 
 	$(INSTALL_DIR) $(1)/usr/libexec/rpcd/
-	$(INSTALL_DATA) ./files/luci.adguardhome $(1)/usr/libexec/rpcd/
+	$(INSTALL_BIN) ./files/luci.adguardhome $(1)/usr/libexec/rpcd/
 
 	$(INSTALL_DIR) $(1)/usr/share/luci/menu.d/
 	$(INSTALL_DATA) ./files/luci-menu.d.json $(1)/usr/share/luci/menu.d/luci-app-adguardhome.json
--- htdocs/config.js
+++ htdocs/config.js
@@ -4,19 +4,6 @@
 'require view';
 
 return view.extend({
-	set_passwd: rpc.declare({
-		object: 'luci.adguardhome',
-		method: 'set_passwd',
-		params: [ 'username', 'hash' ],
-	}),
-
-	change_password: function() {
-		var username = document.getElementById("widget.cbid.adguardhome.config.web_username").value;
-		var password = document.getElementById("widget.cbid.adguardhome.config.web_password").value;
-		var hash = TwinBcrypt.hashSync(password);
-		return Promise.all([ this.set_passwd( username, hash ) ]);		
-	},
-
 	render: function () {
 		// A basic configuration form; the luci.adguardhome script that
 		// powers the other UI pages needs a username and password to
@@ -38,15 +25,27 @@ return view.extend({
 		o = s.option(form.Value, 'web_password', _('Password for AdGuard Home'), _('The password you configured when you set up AdGuard Home'));
 		o.password = true;
 
-		o = s.option(form.Button, 'change_password', _('Change Password'));
-		o.onclick = this.change_password;
+		var ChangePassword = form.Button.extend({
+			set_passwd: rpc.declare({
+				object: 'luci.adguardhome',
+				method: 'set_passwd',
+				params: [ 'username', 'hash' ],
+			}),
+			onclick: function() {
+				var username = document.getElementById("widget.cbid.adguardhome.config.web_username").value;
+				var password = document.getElementById("widget.cbid.adguardhome.config.web_password").value;
+				var hash = TwinBcrypt.hashSync(password);
+				return Promise.all([ this.set_passwd( username, hash ) ]);
+			},
+		});
+		o = s.option(ChangePassword, 'change_password', _('Change Password'));
 
-		var Scriptable = form.DummyValue.extend({
+		var BCryptInclude = form.DummyValue.extend({
 			renderWidget: function(section_id, option_index, cfgvalue) {
 				return E('script', { 'type':'text/javascript', 'src':'/luci-static/resources/view/twin-bcrypt.min.js' });
 			}
 		});
-		o = s.option(Scriptable, 'misc');
+		o = s.option(BCryptInclude, 'misc');
 
 		return m.render();
 	},
